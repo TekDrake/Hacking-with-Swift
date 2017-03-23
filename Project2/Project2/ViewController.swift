d10bc4cbd3d8feffa440689e8aeafded55ea4c5b
Juan Garcia <juansftdev@gmail.com>
2017-03-22 22:55:53 -0300

    Added project 2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Juan Garcia on 3/22/17.
+//  Copyright © 2017 Juan Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
