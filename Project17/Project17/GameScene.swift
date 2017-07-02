8a9262655bbb46a89badb902d787fcdca1df6e55
Juan Garcia <juansftdev@gmail.com>
2017-07-02 23:27:02 +0100

    Setting up

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Juan Garcia on 7/2/17.
+//  Copyright © 2017 Juan Garcia. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
